8042c15a5f6b58a864eea073e057aab23dd9e960
manasux <amr13mk@gmail.com>
2021-10-05 10:47:42 +0530

    Added flipped card

--- src/UI/TeamCard.vue
+++ src/UI/TeamCard.vue
@@ -5,9 +5,14 @@
         <img src="https://i.ibb.co/yQgh3VN/Rectangle-25.png" alt="..." />
       </div>
       <div class="flip_card_back">
-        <h1>John Doe</h1>
-        <p>Architect and Engineer</p>
-        <p>We love that guy</p>
+        <span class="team_person_name">Manas Mishra</span>
+        <span class="team_person_designation">Web Dev Head</span>
+        <span class="team_person_detail"
+          >Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </span>
       </div>
     </div>
   </div>
@@ -32,29 +37,49 @@ export default {};
   text-align: center;
   transition: transform 0.6s;
   transform-style: preserve-3d;
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 }
 .teamcard:hover .flip_card_inner {
   transform: rotateY(180deg);
 }
-.flip_card_front, .flip_card_back {
+.flip_card_front,
+.flip_card_back {
   position: absolute;
   width: 100%;
   height: 100%;
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
 }
-.flip_card_front > img{
-    width: 100%;
-    height: 100%;
+.flip_card_front > img {
+  width: 100%;
+  height: 100%;
 }
 .flip_card_front {
   /* background-color: #bbb; */
   color: black;
 }
 .flip_card_back {
-  background-color: #2980b9;
+  background-color: var(--black);
+  border: 1px solid var(--white);
+  border-radius: 10px;
   color: white;
   transform: rotateY(180deg);
+  display: flex;
+  flex-direction: column;
+}
+.team_person_name {
+  font-family: var(--ff-viga);
+  font-size: 50px;
+}
+.team_person_designation{
+    font-size: 20px;
+    font-family: var(--ff-raleway);
+}
+.team_person_detail{
+    font-size: 18px;
+    font-family: var(--ff-raleway);
+    margin-right: 5%;
+    margin-left: 5%;
+    margin-top: 10%;
 }
 </style>
