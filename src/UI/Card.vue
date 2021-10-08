9e5625cb1042ddb2078ad9b80dd20b451f06d897
manasux <amr13mk@gmail.com>
2021-10-08 10:20:25 +0530

    made speakers page responsive

--- src/UI/Card.vue
+++ src/UI/Card.vue
@@ -28,58 +28,6 @@ export default {
 </script>
 
 <style scoped>
-.card {
-  width: 80%;
-  height: 100%;
-  background-color: var(--card-bg);
-  display: flex;
-}
-.card_img {
-  background-color: pink;
-  width: 35%;
-  height: 100%;
-}
-.card_img > img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-.card_text {
-  width: 65%;
-  display: block;
-}
-.card_text_top {
-  width: 100%;
-  height: 20%;
-  background-color: var(--primary-red);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-.card_text_name {
-  font-family: var(--ff-viga);
-  font-size: 25px;
-  margin-left: 5%;
-}
-.card_text_name_detail {
-  font-family: var(--ff-raleway);
-  font-size: 15px;
-  margin-left: 5%;
-}
-.card_text_bottom {
-  width: 100%;
-  height: 80%;
-  display: flex;
-  flex-wrap: wrap;
-}
-.card_text_bottom > span {
-  color: var(--black);
-  /* display: flex;
-    flex-wrap: wrap; */
-  font-family: var(--ff-roboto);
-  font-size: 20px;
-  margin-left: 5%;
-  margin-right: 5%;
-  margin-top: 2%;
-}
+@import "card.css";
+
 </style>
